b5778a57c884e2ae43c051e47b5c22d21b3e33e6
Vlad Catalin Duma <duma.vlad@yahoo.com>
2023-10-05 21:06:25 +0300

    05.10

--- intro/urls.py
+++ intro/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from intro import views
 
 urlpatterns = [
-    path('first-page/', views.index, name='first-page'),
+    path('first_page/', views.index, name='first-page'),
 ]
